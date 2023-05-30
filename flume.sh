c63b3dc9aa6be0a4d113024b26273520609c0ad4
cesargg6 <133994020+cesargg6@users.noreply.github.com>
2023-05-30 16:24:56 +0200

    Update flume.sh

--- flume.sh
+++ flume.sh
@@ -4,6 +4,8 @@ leerArchivo() {
     rutaCompleta=$1
     while IFS= read -r linea
     do
+        # Ejecutar el script Pig
+        pig -x mapreduce -f /content/pruebas.pig -param input_path='/content/HotelBookings.csv'
         echo "$linea"
     done < "$rutaCompleta"
 }
